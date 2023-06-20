a7431968f02332e98a2a70044bff8770ab2f6fab
tprashant-21 <prashanttimalsina21@gmail.com>
2023-06-20 16:35:03 -0400

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const keys = document.querySelectorAll('.key');
+var cat = document.getElementById("cat");
 
-const audio = document.getElementsByTagName('audio');
+function playSound(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // always a good thing to remove uncessary steps, if it ain't in the list of divs, don't proceed.
+    if(!key) return;
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+    cat.classList.add('shake');
+    //make a habit of thinking edge cases, here we want to play the sound everytime, we don't want to wait for the play sound to finish
+    audio.currentTime = 0;
+    audio.play();
+}
 
-window.addEventListener('keydown', function(e) {
-    for (key of keys){
-        if(key.getAttribute("data-key") == e.keyCode){
-            key.classList.add("playing");
-            for (sound of audio){
-                if (sound.getAttribute("data-key") == e.keyCode){
-                    sound.play();
-                }
-            }
-        }
-    }
-});
+function removeStyle(e){
+    //there are many transition, we want the longest one in the button element that is transform.
+    if(e.propertyName !== "transform") return;
+    e.target.classList.remove('playing');
+    cat.classList.remove('shake');
+}
 
-window.addEventListener('keyup', function(e) {
-    for (key of keys){
-        key.classList.remove("playing");  
-    }
-});
+const letters = document.getElementsByClassName("key");
 
+// added EventListener when the transitions end on CSS for the element
+Array.from(letters).forEach(key =>
+    key.addEventListener("transitionend", removeStyle)
+);
+
+// added EventListener whenever a keyboard is pressed
+window.addEventListener('keydown', playSound);
